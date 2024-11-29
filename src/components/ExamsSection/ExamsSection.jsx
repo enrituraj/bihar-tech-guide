450be52a6c1b95b58f292cbf588b9dec6f7db000
Ritu <rn26rituraj@gmail.com>
2024-11-29 11:56:50 +0530

    creatng singin and sinup up page and done with userside validation

--- src/components/ExamsSection/ExamsSection.jsx
+++ src/components/ExamsSection/ExamsSection.jsx
@@ -48,7 +48,7 @@ const ExamsSection = () => {
         <div
           className="
               grid 
-              grid-cols-1
+              grid-cols-2
               sm:grid-cols-2 
               md:grid-cols-3 
               lg:grid-cols-6 
